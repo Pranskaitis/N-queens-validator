e1f8ca964558f6c9a904fd362e469257414bd2e9
Mantas Pranskaitis <52334614+Pranskaitis@users.noreply.github.com>
2019-07-24 15:30:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int qcheck(int arr[], int N)
+int qcheck(int arr[], int N) // a function queen check, takes the number of queens N and the positions of them arr[]
 {
-    int posx1[N*N],posx2[N*N],posy1[N*N],posy2[N*N];
-    int i=0,j,errcount=0;
+    int posx1[N*N],posx2[N*N],posy1[N*N],posy2[N*N]; // used to store the positions of intercepting queens
+    int i=0,j,errcount=0; // errcount means how many interceptions there are
     for (i=0;i<N-1;++i)
     {
         for (j=i+1;j<N;j++)
         {
-            if(arr[i]==arr[j] || abs(arr[i]-arr[j])==abs(i-j))
+            if(arr[i]==arr[j] || abs(arr[i]-arr[j])==abs(i-j)) // if two queens intercept, store their positions and increase
+                                                               // the errcount
             {
                 posx1[errcount]=i+1;
                 posx2[errcount]=j+1;
@@ -19,15 +20,15 @@ int qcheck(int arr[], int N)
             }
         }
     }
-    if (errcount==0)
+    if (errcount==0) // if no queens intercept
     {
         printf("TRUE");
     }
-    if (errcount==1)
+    if (errcount==1) // if only one pair of queens intercept
     {
         printf("FALSE, two queens intercept each other: (%d,%d) and (%d,%d)",posx1[0],posy1[0],posx2[0],posy2[0]);
     }
-    if (errcount>1)
+    if (errcount>1) // if more that one pair of queens intercept
     {
         printf("FALSE, there are %d number of interceptions \n",errcount);
         for (i=0;i<errcount;++i)
@@ -42,7 +43,7 @@ int main()
     int N, i=0;
     printf("Enter number of queens \n");
     scanf("%d",&N);
-    if (N==1 || N==2)
+    if (N==1 || N==2) // the trivial cases
     {
         printf("NO");
         return 0;
